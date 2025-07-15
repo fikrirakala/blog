2c89c3d470f3194d5459520627a9571a7b5cc9e5
Fikri Rakala <fikrirakala@gmail.com>
2025-07-15 12:31:43 +0800

    Update Home

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="px-4 py-6 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-3xl">
-        <p>&copy; 2025 Fikri Rakala</p>
+        <p className="text-muted-foreground">&copy; 2025 Fikri Rakala</p>
       </div>
     </footer>
   );
